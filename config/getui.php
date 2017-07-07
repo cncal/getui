f7394e96ba9c2beb04a217fc4a2a944309f63fb9
cncal <flycalvin@qq.com>
2017-07-07 10:34:02 +0800

    remove private info from config/getui.php

--- config/getui.php
+++ config/getui.php
@@ -6,9 +6,9 @@ return [
      */
     'basic_info' => [
         'host' => "http://sdk.open.api.igexin.com/apiex.htm",
-        'app_id' => "A2jmZ6lrWw5VWQ5SPGr258",
-        'app_key' => "wfOhPKrLLK9zzcTtPM86z5",
-        'master_secret' => "cUh0xehU259Zz6AB92Rh23",
+        'app_id' => "",
+        'app_key' => "",
+        'master_secret' => "",
     ],
 
     /**
@@ -16,7 +16,7 @@ return [
      */
     'push_info' => [
         'is_offline' => true,
-        'offline_expire_time' => 3600*1000*2, // 单位为毫秒
+        'offline_expire_time' => 3600 * 1000 * 2, // 单位为毫秒
         'network_type' => 0 // 设置是否根据WIFI推送消息, 0为不限制推送, 1为wifi推送, 2为4G/3G/2G
     ],
 
